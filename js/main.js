b1f46df3d6e382a4d5852edcb61cad0f049445bc
Rafael <rafael.amoyan@mail.ru>
2020-06-07 20:33:12 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+    
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true
+    });
+
 
 });
